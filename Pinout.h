a015f7e56022cab4093f2c139817f309ca87f205
GameDreamSt <gudauskasdovydas@gmail.com>
2025-10-12 15:57:10 +0300

    Pinout clarification

--- Pinout.h
+++ Pinout.h
@@ -10,7 +10,7 @@
 
 // DIGITAL OUTPUT
 #define PIN_PRECHARGE 32        // G32 P1 Precharge relay control
-#define PIN_POS_CONTACTOR 21    // SP=G36 P2 Positive contactor control
+#define PIN_POS_CONTACTOR 21    // G21 P2 Positive contactor control
 #define PIN_NEG_CONTACTOR 33    // G33 P3 Negative contactor control
 #define PIN_VACUUM_PUMP 14      // G14 P4 Vacuum pump activation
 #define PIN_WATER_PUMP 25       // G25 P5 Water pump activation
@@ -18,11 +18,11 @@
 #define PIN_CHARGER_IGN 26      // G26 P7 Charger turn on signal
 // G13 P8 unused
 
-#define MKRCAN_MCP2515_CS_PIN 2 // Slave Select pin (SS) /	Chip Select Pin (CS)
+#define MKRCAN_MCP2515_CS_PIN 2  // Slave Select pin (SS) /	Chip Select Pin (CS)
 #define MKRCAN_MCP2515_INT_PIN 5 // Interrupt pin for CAN messages (not used)
 
 // ANALOG TO DIGITAL
-#define APIN_VACUUM 35  // G35 Vacuum sensor
-#define APIN_Throttle1 39 // G39=SN Analog throttle channel 1
-#define APIN_Throttle2 34 // G34 Analog throttle channel 2
-#define APIN_CHARGER_PP -1 // Proximity Pilot
+#define APIN_VACUUM 35          // G35 Vacuum sensor
+#define APIN_Throttle1 39       // G39=SN Analog throttle channel 1
+#define APIN_Throttle2 34       // G34 Analog throttle channel 2
+#define APIN_CHARGER_PP 36      // G36=SP Proximity Pilot
